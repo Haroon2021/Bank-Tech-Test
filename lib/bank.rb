5371e54734682f5e7ddb143127a23f9731c130bf
Haroon <haroon20@hotmail.co.uk>
2022-03-29 16:52:26 +0100

    Statement array reordered to match output

--- lib/bank.rb
+++ lib/bank.rb
@@ -8,24 +8,25 @@ class Bank
 
   def deposit(amount_deposited,date)
     @balance += amount_deposited
-    @statementarray.push([date, amount_deposited , " ", @balance]) 
+    @statementarray.insert(0,[date, " || ", amount_deposited , " ||   || ", @balance]) 
   end
 
   def widthdrew(amount_widthdrew,date)
     @balance -= amount_widthdrew
-    @statementarray.push([date, " ", amount_widthdrew, @balance]) 
+    @statementarray.insert(0,[date," ||   || ", amount_widthdrew," || ", @balance]) 
   end
 
 # Statement mentod here for testing purposes
   def statement
     @statementarray 
   end
-# To work on
-  # def printstatement
-  #   numberoftransactions = @statementarray[0].length
-  #   @statementarray.each do |item|
-  #     puts @statementarray[numberoftransactions][0]
-  #   end
-  # end
+
+  def printstatement
+    puts "date || credit || debit || balance"
+    @statementarray.reverse
+    @statementarray.each do |item|
+      puts item
+    end
+  end
 
 end
