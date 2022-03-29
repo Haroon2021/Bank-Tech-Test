2cdc6d7c2b599ffea09c4309c52c738cdea9ffcf
Haroon <haroon20@hotmail.co.uk>
2022-03-29 16:18:26 +0100

    refactored deposit and widthdrewal methods

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,25 +1,19 @@
 class Bank
   def initialize
     @balance = 0
-    @statementarray = [[],[],[],[]]
+    @statementarray = []
   end
 
   attr_reader :balance
 
   def deposit(amount_deposited,date)
     @balance += amount_deposited
-    @statementarray[1].push(amount_deposited)
-    @statementarray[2].push(" ")
-    @statementarray[3].push(@balance)
-    @statementarray[0].push(date)
+    @statementarray.push([date, amount_deposited , " ", @balance]) 
   end
 
   def widthdrew(amount_widthdrew,date)
     @balance -= amount_widthdrew
-    @statementarray[2].push(amount_widthdrew)
-    @statementarray[1].push(" ")
-    @statementarray[3].push(@balance)
-    @statementarray[0].push(date)
+    @statementarray.push([date, " ", amount_widthdrew, @balance]) 
   end
 
 # Statement mentod here for testing purposes
